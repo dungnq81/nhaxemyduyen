1e02ecfdadd5a6bb51d8ecc772dae6be0d90b490
cuongtran <cuongnt392003@gmail.com>
2025-02-22 12:55:54 +0700

    update

--- main.js
+++ main.js
@@ -25,3 +25,37 @@ const menuButton=document.querySelector("#menu-btn");
 menuButton.addEventListener("click",()=>{
     menu.classList.toggle("hidden");
 })
+
+const slides = document.getElementById('slides');
+const dots = document.querySelectorAll('.dot');
+const totalSlides = 4;
+let currentSlide = 0;
+
+// Bắt sự kiện khi nhấn vào dấu chấm
+dots.forEach((dot, index) => {
+  dot.addEventListener('click', () => {
+    currentSlide = index;
+    updateSlide();
+  });
+});
+
+function updateSlide() {
+  slides.style.transform = `translateX(-${currentSlide * 100}%)`;
+  updateDots();
+}
+
+// Cập nhật dấu chấm đang được chọn
+function updateDots() {
+  dots.forEach((dot, index) => {
+    if (index === currentSlide) {
+      dot.classList.add('bg-red-500');
+      dot.classList.remove('bg-gray-400');
+    } else {
+      dot.classList.add('bg-gray-400');
+      dot.classList.remove('bg-red-500');
+    }
+  });
+}
+
+// Đặt slide đầu tiên được chọn khi tải trang
+updateDots();
